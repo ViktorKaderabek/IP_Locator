55551776c58a1377e5922498f6083a0f7eb4bfab
viktorkaderabek <viktorkaderabekpc@seznam.cz>
2022-06-14 22:55:13 +0200

    smt

--- app/src/main/java/com/example/ipwho/presentation/MyIpScreen/MyIpScreen.kt
+++ app/src/main/java/com/example/ipwho/presentation/MyIpScreen/MyIpScreen.kt
@@ -1,6 +1,7 @@
 package com.example.ipwho.presentation.MyIpScreen
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -26,49 +27,182 @@ fun MyIpScreen() {
         verticalArrangement = Arrangement.Center
     ) {
         state.IpInfo?.let { result ->
-            Text(
-                text = "IP : ${result.ip}",
-                color = Color.White,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-                fontStyle = FontStyle.Italic,
-                modifier = Modifier
-                    .padding(20.dp)
-            )
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                item {
+                    Text(
+                        text = result.flag.emoji,
+                        color = Color.White,
+                        fontSize = 50.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontStyle = FontStyle.Italic,
+                        modifier = Modifier
+                            .padding(20.dp)
+                    )
 
-            Text(
-                text = "Success : ${result.success}",
-                color = if (result.success) Color.Green else Color.Red,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-                fontStyle = FontStyle.Italic,
-                modifier = Modifier
-                    .padding(20.dp)
-            )
+                    Text(
+                        text = "IP : ${result.ip}",
+                        color = Color.White,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontStyle = FontStyle.Italic,
+                        modifier = Modifier
+                            .padding(20.dp)
+                    )
 
-            Text(
-                text = "Type : ${result.type}",
-                color = Color.White,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-                fontStyle = FontStyle.Italic,
-                modifier = Modifier
-                    .padding(20.dp)
-            )
+                    Text(
+                        text = "Success : ${result.success}",
+                        color = if (result.success) Color.Green else Color.Red,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontStyle = FontStyle.Italic,
+                        modifier = Modifier
+                            .padding(20.dp)
+                    )
 
-            Text(
-                text = "IP : ${result.ip}",
-                color = Color.White,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-                fontStyle = FontStyle.Italic,
-                modifier = Modifier
-                    .padding(20.dp)
-            )
+                    Text(
+                        text = "Type : ${result.type}",
+                        color = Color.White,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontStyle = FontStyle.Italic,
+                        modifier = Modifier
+                            .padding(20.dp)
+                    )
+
+                    Text(
+                        text = "Continent : ${result.continent}",
+                        color = Color.White,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontStyle = FontStyle.Italic,
+                        modifier = Modifier
+                            .padding(20.dp)
+                    )
+
+                    Text(
+                        text = "Continent code : ${result.continent_code}",
+                        color = Color.White,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontStyle = FontStyle.Italic,
+                        modifier = Modifier
+                            .padding(20.dp)
+                    )
+
+                    Text(
+                        text = "Country : ${result.country}",
+                        color = Color.White,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontStyle = FontStyle.Italic,
+                        modifier = Modifier
+                            .padding(20.dp)
+                    )
+
+                    Text(
+                        text = "Country code : ${result.country_code}",
+                        color = Color.White,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontStyle = FontStyle.Italic,
+                        modifier = Modifier
+                            .padding(20.dp)
+                    )
+
+                    Text(
+                        text = "Region : ${result.region}",
+                        color = Color.White,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontStyle = FontStyle.Italic,
+                        modifier = Modifier
+                            .padding(20.dp)
+                    )
+
+                    Text(
+                        text = "City : ${result.city}",
+                        color = Color.White,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontStyle = FontStyle.Italic,
+                        modifier = Modifier
+                            .padding(20.dp)
+                    )
+
+                    Text(
+                        text = "is EU : ${result.is_eu}",
+                        color = if (result.is_eu) Color.Green else Color.Red,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontStyle = FontStyle.Italic,
+                        modifier = Modifier
+                            .padding(20.dp)
+                    )
+
+                    Text(
+                        text = "Postal : ${result.postal}",
+                        color = Color.White,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontStyle = FontStyle.Italic,
+                        modifier = Modifier
+                            .padding(20.dp)
+                    )
+
+                    Text(
+                        text = "Calling code : ${result.calling_code}",
+                        color = Color.White,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontStyle = FontStyle.Italic,
+                        modifier = Modifier
+                            .padding(20.dp)
+                    )
+
+                    Text(
+                        text = "Capital : ${result.capital}",
+                        color = Color.White,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontStyle = FontStyle.Italic,
+                        modifier = Modifier
+                            .padding(20.dp)
+                    )
+
+                    Text(
+                        text = "Borders : ${result.borders}",
+                        color = Color.White,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontStyle = FontStyle.Italic,
+                        modifier = Modifier
+                            .padding(20.dp)
+                    )
+
+
+
+                }
+            }
 
         }
 
